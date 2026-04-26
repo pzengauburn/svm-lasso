2426d06c04e9902db6087c36380257e7152426fd
Peng Zeng <45214283+pzengauburn@users.noreply.github.com>
2026-04-26 05:35:42 -0500

    Create mydef.h

---
+++ codes/lassoCD/mydef.h
@@ -0,0 +1,27 @@
+/********************************************************************
+ * some constants
+ * Thursday, July 08, 2010
+ ********************************************************************/
+
+#ifndef ZP_DEF
+#define ZP_DEF
+
+#include <R.h>
+#include <Rmath.h>
+#include <R_ext/BLAS.h>
+#include <R_ext/Lapack.h>
+
+const int ZERO = 0;
+const int ONE  = 1;
+
+const double dZERO =  0.0;
+const double dONE  =  1.0;
+const double dnONE = -1.0;
+
+const double EPS = 1e-12;
+
+#endif
+
+/********************************************************************
+ * END OF THE FILE.
+ ********************************************************************/
